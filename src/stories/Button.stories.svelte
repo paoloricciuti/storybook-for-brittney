62ae157a8c191fff7ded66184747dd9a4a8e1116
paoloricciuti <ricciutipaolo@gmail.com>
2024-04-25 22:59:57 +0200

    how to test

--- src/stories/Button.stories.svelte
+++ src/stories/Button.stories.svelte
@@ -13,11 +13,12 @@
 
 <script lang="ts">
 	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import { within, expect, fn, userEvent } from '@storybook/test';
 	import Button from './Button.svelte';
 </script>
 
 <Template id="main" let:args>
-	<Button {...args} />
+	<Button {...args} on:click={args.click} />
 </Template>
 
 <Story
@@ -27,6 +28,19 @@
 		primary: false,
 		backgroundColor: '#bada55',
 		size: 'small',
-		label: 'Cool'
+		label: 'Cool',
+		click: fn() // <-- this create a mock function that you can expect on (args, how many time is called etc)
+	}}
+	play={async ({ canvasElement, args, step }) => {
+		const canvas = within(canvasElement); // <-- this allow you to query by role, label etc (like testing library)
+		const button = await canvas.findByRole('button');
+		await step("button it's in the document", () => {
+			// <-- with step you perform steps in the interaction
+			expect(button).toBeInTheDocument();
+		});
+		await step('clicking it send a click event', async () => {
+			await userEvent.click(button); // <-- perform user actions with userEvent from @storybook/test
+			expect(args.click).toHaveBeenCalled();
+		});
 	}}
 ></Story>
