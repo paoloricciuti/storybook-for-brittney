a59a9ac42fcedbd416b681c68b18a8b44bdf9f28
paoloricciuti <ricciutipaolo@gmail.com>
2024-04-25 22:50:05 +0200

    init storybook

---
+++ src/stories/Button.stories.svelte
@@ -0,0 +1,32 @@
+<script lang="ts" context="module">
+	import type { Meta } from '@storybook/svelte';
+	export const meta: Meta = {
+		title: 'Button',
+		argTypes: {
+			size: {
+				control: 'select',
+				options: ['small', 'medium', 'large']
+			}
+		}
+	};
+</script>
+
+<script lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+</script>
+
+<Template id="main" let:args>
+	<Button {...args} />
+</Template>
+
+<Story
+	name="Primary"
+	template="main"
+	args={{
+		primary: false,
+		backgroundColor: '#bada55',
+		size: 'small',
+		label: 'Cool'
+	}}
+></Story>
